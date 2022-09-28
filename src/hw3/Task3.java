31c508257def69fc17aa91c0d06ddeebd8e59908
v.sukhenko <v.sukhenko@just-ai.com>
2022-09-28 21:39:59 +0300

    scanner & task 3 fixes

--- src/hw3/Task3.java
+++ src/hw3/Task3.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Загадайте число от 2 до 100");
+        System.out.println("Загадайте число от 2 до 200");
         int userNumber = in.nextInt();
-        for (int low = 2, high = 200, result = 0; result == 0;) {
+        while (userNumber<2||userNumber>200) {
+            System.out.println("Это число не подходит. Загадайте число от 2 до 200");
+            userNumber = in.nextInt();
+        }
+        for (int low = 1, high = 201, result = 0; result == 0;) {
             int mid = (high + low) / 2;
             if (mid < userNumber) {
                 low = mid;
