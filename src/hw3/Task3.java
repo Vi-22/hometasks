67219b2815271e4c6edb018ad1b4ba64c2aa004d
v.sukhenko <v.sukhenko@just-ai.com>
2022-09-30 22:15:33 +0300

    task 3 correction

--- src/hw3/Task3.java
+++ src/hw3/Task3.java
@@ -13,13 +13,19 @@ public class Task3 {
         }
         for (int low = 1, high = 201, result = 0; result == 0;) {
             int mid = (high + low) / 2;
-            if (mid < userNumber) {
-                low = mid;
-            } else if (mid > userNumber) {
-                high = mid;
+            System.out.println("Это число " + mid + "?");
+            int yesOrNo= in.nextInt();
+            if  (yesOrNo ==0) {
+                System.out.println("Это число больше?");
+                yesOrNo= in.nextInt();
+                if  (yesOrNo ==0) {
+                    high = mid;
+                } else {
+                    low = mid;
+                }
             } else {
-                result = 1;
-                System.out.println("Ваше число: " + mid);
+                 result = 1;
+                 System.out.println("Ваше число: " + mid);
             }
         }
         in.close();
