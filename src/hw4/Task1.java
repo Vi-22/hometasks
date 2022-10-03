06cc910be99626a02a296e0c4983762bb7427679
v.sukhenko <v.sukhenko@just-ai.com>
2022-10-03 22:11:42 +0300

    ref task 1

--- src/hw4/Task1.java
+++ src/hw4/Task1.java
@@ -1,26 +1,24 @@
 package hw4;
-
+// на самом деле это hw5
 import java.util.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
         int[] array1 = new int[(int) (Math.random() * 20)]; //создаем случайный массив до с длинной от 1 до 20
         int sum = 0;//сразу создаем переменную для суммы элементов массива
-        for (int i = 0; i < array1.length; i++) {
-            array1[i] += (int) (Math.random() * 100);//создаем случайные значения от 1 до 100
-            sum += array1[i]; // сразу считаем сумму элементов массива
-        }
-        System.out.println("Вот такой массив: " + Arrays.toString(array1));
         int max = array1[0];
         int min = array1[0];
         for (int i = 0; i < array1.length; i++) {
+            array1[i] += (int) (Math.random() * 100);//создаем случайные значения от 1 до 100
+            sum += array1[i]; // сразу считаем сумму элементов массива
             if (array1[i] > max) {
                 max = array1[i];
             }
             if (array1[i] < min) {
-               min = array1[i];
-               }
+                min = array1[i];
             }
+        }
+        System.out.println("Вот такой массив: " + Arrays.toString(array1));
         System.out.println("Минимальное значение: " + min);
         System.out.println("Максимальное значение: " + max);
         System.out.println("Среднее арифметическое: " + (sum / array1.length));
